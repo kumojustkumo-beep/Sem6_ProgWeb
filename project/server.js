af90ce909a7c32976023fe8f20e7c368571b889a
kumojustkumo-beep <kumojustkumo@gmail.com>
2026-04-17 11:43:47 -0400

    Update server.js

--- project/server.js
+++ project/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("publico"));
+app.use(express.static("public"));
 
 const ARCHIVO_DATOS = path.join(__dirname, "data", "datos.json");
 
